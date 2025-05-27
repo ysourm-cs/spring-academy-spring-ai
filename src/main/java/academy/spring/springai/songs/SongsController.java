66ece0de4b726d47a368d375a04ee07bf72f7f84
= <=>
2025-05-27 20:34:17 +0000

    "spring-ai short guide"

--- src/main/java/academy/spring/springai/songs/SongsController.java
+++ src/main/java/academy/spring/springai/songs/SongsController.java
@@ -18,16 +18,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/songs")
 public class SongsController {
 
-	// Inject ChatClient
+	public SongsController(ChatClient client) {
+		this.chatClient = client;
+	}
 
+	// Inject ChatClient
+	private final ChatClient chatClient;
 
 	// String Prompt
-
+	@GetMapping("/stringprompt/topSong")
+	public String topSong() {
+		String stringPrompt = "What was the Billboard number one year-end top 100 single for 1984?";
+		return this.chatClient.call(stringPrompt);
+	}
 
 	// Prompt with parameter
-
-
+	@GetMapping("/parameter/topsong/{year}")
+	public String parameterTopSong(@PathVariable("year") int year) {
+  	String stringPrompt =
+    		"What was the Billboard number one year-end top 100 single for {year}?";
+	PromptTemplate template = new PromptTemplate(stringPrompt);
+	template.add("year", year);
+	return chatClient.call(template.render());		
+	}	
 	// Return object
+	@GetMapping("/objectreturn/topsong/{year}")
+	public TopSong objectReturnTopSong(@PathVariable("year") int year) {
+		BeanOutputParser<TopSong> parser = new BeanOutputParser<>(TopSong.class);
+		String stringPrompt=
+		"""
+			What was the Billboard number one year-end top 100 single for {year}?
+			{format}		
+				""";
+		
+		PromptTemplate template = new PromptTemplate(stringPrompt);
+		template.add("year", year);
+		template.add("format", parser.getFormat());
 
+		//System.out.println("PARSER FORMAT: " + parser.getFormat());
+		Prompt prompt = template.create();
+		Generation generation = chatClient.call(prompt).getResult();
+
+		TopSong topSong = parser.parse(generation.getOutput().getContent());
+		return topSong;
+	}
 
 }
